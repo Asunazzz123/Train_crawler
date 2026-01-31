082e996c5c79f7839a9859d2baabff5c242fb51c
Yuuko <z2583636628@163.com>
2026-02-01 01:41:45 +0800

    Update Backend "sqlite interact" & Add Metro related data

--- backend/utils/constant.py
+++ backend/utils/constant.py
@@ -7,4 +7,5 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 RESOURCE_DIR = BASE_DIR / 'resource'
 JS_DIR = RESOURCE_DIR / 'js' / 'framework'
 JSON_DIR = RESOURCE_DIR / 'json'
-
+METRO_JSON_DIR = JSON_DIR / 'metro'
+RAIL_JSON_DIR = JSON_DIR / 'rail'
